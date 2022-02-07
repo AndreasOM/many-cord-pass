e51b5c93e4b8af3508ab33da00d49a3475baa929
Andreas Neukoetter <andreas@omni-mad.com>
2022-02-07 21:15:03 +0000

    + Add automatic discovery of a stream deck.

--- src/mcp_main.rs
+++ src/mcp_main.rs
@@ -1,8 +1,39 @@
+
+fn find_deck() -> (u16, u16, Option<String>) {
+    let hid = hidapi::HidApi::new().expect("could not connect to hidapi");
+    let device = hid
+        .device_list()
+        //.filter(|item| item.product_id() == 0x006d)
+        .filter(|item|{
+            if let Some( product ) = &item.product_string() {
+              dbg!(&product);
+                product.contains( "Stream Deck" )
+            } else {
+                false
+            }
+            //dbg!(&item); item.product_id() == 0x006d
+        })
+        .next()
+        .expect("Could not find Streamdeck");
+
+    println!(
+        "Attempting to connect to {:?}. vid: {:?}, pid: {:?}, serial: {:?}",
+        device.product_string(),
+        device.vendor_id(),
+        device.product_id(),
+        device.serial_number(),
+    );
+
+    (
+        device.vendor_id(),
+        device.product_id(),
+        device.serial_number().map(|str| String::from(str)),
+    )
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let pid = 0x0063;
-    let vid = 0x0fd9;
-    let serial = None;
+    let( vid, pid, serial ) = find_deck();
 
     let mut deck = match streamdeck::StreamDeck::connect(vid, pid, serial) {
         Ok(d) => d,
@@ -12,10 +43,14 @@ async fn main() -> anyhow::Result<()> {
         }
     };
 
-    let key = 0;
+    let version = deck.version()?;
+    println!("Firmware Version: {}", &version );
+
+    let key = 4;
     let file = "data/about.png";
     let opts = streamdeck::images::ImageOptions::default();
 
+    println!("Setting button {} to {}", key, &file);
     deck.set_button_file(key, &file, &opts)?;
 
     Ok(())
