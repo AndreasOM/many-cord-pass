ad1cc3f44f95a31d750b052e392edcf24170574f
Andreas Neukoetter <andreas@omni-mad.com>
2022-09-30 12:18:16 +0200

    + Allow env vars in event configs.

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use serde::Deserialize;
 use serde_yaml;
 
@@ -40,33 +41,75 @@ impl Config {
 	pub fn from_file(filename: &str) -> anyhow::Result<Config> {
 		let f = std::fs::File::open(&filename)?;
 		let mut c: Config = serde_yaml::from_reader(&f)?;
-		/*
-				if let Some(f) = &c.author_config_file {
-					c.author_config = AuthorConfig::from_file(&f)?;
-				}
-
-				if let Some(f) = &c.tag_config_file {
-					c.tag_config = TagConfig::from_file(&f)?;
-				}
-		*/
+		c.expand()?;
 		Ok(c)
 	}
-
-	/*
-		pub fn author_config(&self) -> &AuthorConfig {
-			&self.author_config
-		}
-
-		pub fn tag_config(&self) -> &TagConfig {
-			&self.tag_config
-		}
-	*/
 
 	pub fn buttons(&self) -> &Vec<ButtonConfig> {
 		&self.buttons
 	}
 	pub fn pages(&self) -> &Vec<PageConfig> {
 		&self.pages
+	}
+
+	pub fn expand(&mut self) -> anyhow::Result<()> {
+		for b in self.buttons.iter_mut() {
+			b.expand()?;
+		}
+		for p in self.pages.iter_mut() {
+			p.expand()?;
+		}
+		Ok(())
+	}
+}
+
+impl PageConfig {
+	pub fn expand(&mut self) -> anyhow::Result<()> {
+		Ok(())
+	}
+}
+impl ButtonConfig {
+	pub fn expand(&mut self) -> anyhow::Result<()> {
+		if let Some(ec) = &mut self.on_press {
+			ec.expand()?;
+		}
+		if let Some(ec) = &mut self.on_release {
+			ec.expand()?;
+		}
+		Ok(())
+	}
+}
+impl EventConfig {
+	pub fn expand(&mut self) -> anyhow::Result<()> {
+		let re_env = Regex::new(r"^(.*)(\$\{env:(.+)\})(.*)$").unwrap();
+		if let Some(actions) = &mut self.actions {
+			for a in actions.iter_mut() {
+				let old_a = a.clone();
+				let mut num_of_expansions = 0;
+				let mut any_expansion = true;
+				while any_expansion {
+					any_expansion = false;
+					if let Some(mat) = re_env.captures(a) {
+						//println!("{:?}", &mat);
+						let prefix = &mat[1];
+						let varname = &mat[3];
+						let suffix = &mat[4];
+						let varvalue = std::env::var(varname)
+							.expect(&format!("Env var {} not found", &varname));
+
+						let new_a = prefix.to_string() + &varvalue + suffix;
+						//println!("{} -> {}", &a, &new_a);
+						*a = new_a;
+						num_of_expansions += 1;
+						any_expansion = true;
+					}
+				}
+				if num_of_expansions > 0 {
+					println!("{}\n\t-> {}", &old_a, &a);
+				}
+			}
+		}
+		Ok(())
 	}
 }
 
